d40b6d450d3cf6509f97fc2b60bd5ad8a65f3498
eliotc1986 <shinjukudev@gmail.com>
2020-12-06 22:09:44 +0900

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -235,7 +235,7 @@ class App extends React.PureComponent {
                                 this.setState(
                                   {
                                     phrases: this.state.phrases.filter(
-                                      (phrase) => phraseId !== phraseId,
+                                      (phrase) => phrase.id !== phraseId,
                                     ),
                                   },
                                   () => {
--- src/components/Dialogs/AddTopicDialog/index.js
+++ src/components/Dialogs/AddTopicDialog/index.js
@@ -7,7 +7,6 @@ import {
   TextareaField,
   EditIcon,
   Pane,
-  Paragraph,
   Popover,
   Position,
   Heading,
